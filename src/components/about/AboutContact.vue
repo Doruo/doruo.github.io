9739d3b8d2921e157402bcae6b97d55222e1454c
hayem <marc.haye@etu.umontpellier.fr>
2025-10-22 19:44:43 +0200

    feat: AboutLinks

--- src/components/about/AboutContact.vue
+++ src/components/about/AboutContact.vue
@@ -1,45 +1,14 @@
 <script setup lang="ts">
-import { Pictures } from '@/data/Pictures'
-import { Links } from '@/data/Links'
-import LinkMail from '../links/LinkMail.vue';
+import AboutLinks from './AboutLinks.vue';
 </script>
 
 <template>
   <!-- CONTACT -->
   <section id="contact" class="contact">
     <div class="conteneur">
-
       <h2>Contact</h2>
       <p>Si mon profil correspond à ce que vous recherchez, contactez-moi !</p>
-
-      <div class="social-links">
-
-        <LinkMail :link=Links.mail />
-
-        <div class="social-link-img">
-          <a :href=Links.github>
-            <img alt="github" :src=Pictures.GITHUB>
-          </a>
-        </div>
-
-        <div class="social-link-img">
-          <a :href=Links.linkedin>
-            <img alt="linkedIn" :src=Pictures.LINKEDIN>
-          </a>
-        </div>
-        <div class="social-link-img">
-          <a :href=Links.gravatar>
-            <img alt="gravatar" :src=Pictures.GRAVATAR>
-          </a>
-        </div>
-
-        <div class="social-link-img">
-          <a :href=Links.itchio>
-            <img alt="itchio" :src=Pictures.ITCHIO>
-          </a>
-        </div>
-
-      </div>
+      <AboutLinks/>
     </div>
   </section>
 
---
+++ src/components/about/AboutLinks.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Links } from '@/data/Links';
+import LinkMail from '../links/LinkMail.vue';
+import LinkGithub from '../links/LinkGithub.vue';
+import LinkLinkedin from '../links/LinkLinkedin.vue';
+import LinkGravatar from '../links/LinkGravatar.vue';
+import LinkItchio from '../links/LinkItchio.vue';
+</script>
+
+<template>
+  <div class="social-links">
+    <LinkMail :link=Links.mail />
+    <LinkGithub :link=Links.github />
+    <LinkLinkedin :link=Links.linkedin />
+    <LinkGravatar :link=Links.gravatar />
+    <LinkItchio :link=Links.itchio />
+  </div>
+</template>
