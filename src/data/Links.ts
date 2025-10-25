031afa1d34c724961394236ab89b8a7471ba40f4
hayem <marc.haye@etu.umontpellier.fr>
2025-10-25 16:35:44 +0200

    refactor: simplify link slash

--- src/data/Links.ts
+++ src/data/Links.ts
@@ -2,33 +2,33 @@ export class Links {
 
   // Websites
   static readonly ndi="https://nuitdelinfo.com/"
-  static readonly eldenring="https://fr.bandainamcoent.eu/elden-ring/elden-ring"
+  static readonly eldenring="https://fr.bandainamcoent.eu/elden-ring/elden-ring/"
   static readonly eldenringapi="https://eldenring.fanapis.com/"
   static readonly scodoc="https://scodoc.org/"
 
   // Social
-  static readonly github="https://github.com/doruo"
-  static readonly mail="mailto:marc.antonio.haye@gmail.com"
+  static readonly github="https://github.com/doruo/"
+  static readonly mail="mailto:marc.antonio.haye.pro@gmail.com"
   static readonly linkedin="https://www.linkedin.com/in/marc-haye/"
-  static readonly gravatar="https://fr.gravatar.com/marcantoniohaye"
+  static readonly gravatar="https://fr.gravatar.com/marcantoniohaye/"
   static readonly itchio="https://doruo.itch.io/"
 
   // Github
-  static readonly githubPages=this.github+"/doruo.github.io"
-  static readonly githubEldenbuild=this.github+"/EldenBuild"
-  static readonly githubNdi=this.github+"/NDI-2024-RaceForWater"
-  static readonly githubTrains=this.github+"/Trains"
-  static readonly githubMesh=this.github+"/Mesh"
-  static readonly githubMastermind=this.github+"/mastermind"
-  static readonly githubFalloutdle=this.github+"/falloutdle"
-  static readonly githubGol=this.github+"/GameOfLife"
-  static readonly githubMarinIvre=this.github+"/marinIvre"
-  static readonly githubSudoku=this.github+"/Sudoku"
-  static readonly githubJellybot=this.github+"/JellyBot"
-  static readonly githubPoursuiteEtudes=this.github+"/PoursuiteEtudes"
+  static readonly githubPages=this.github+"doruo.github.io"
+  static readonly githubEldenbuild=this.github+"EldenBuild"
+  static readonly githubNdi=this.github+"NDI-2024-RaceForWater"
+  static readonly githubTrains=this.github+"Trains"
+  static readonly githubMesh=this.github+"Mesh"
+  static readonly githubMastermind=this.github+"mastermind"
+  static readonly githubFalloutdle=this.github+"falloutdle"
+  static readonly githubGol=this.github+"GameOfLife"
+  static readonly githubMarinIvre=this.github+"marinIvre"
+  static readonly githubSudoku=this.github+"Sudoku"
+  static readonly githubJellybot=this.github+"JellyBot"
+  static readonly githubPoursuiteEtudes=this.github+"PoursuiteEtudes"
 
   // Projects
-  static readonly timemagetournament=this.itchio+"/time-mage-tournament"
+  static readonly timemagetournament=this.itchio+"time-mage-tournament"
   static readonly falloutdle="https://falloutdle.net"
 
   // Dev
