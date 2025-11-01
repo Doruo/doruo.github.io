8ebdedb99ed1f55af67be918a37c890c6457340f
hayem <marc.haye@etu.umontpellier.fr>
2025-11-01 10:48:50 +0100

    refactor: remove > in project cards

--- src/components/projects/abstract/ProjectCardItem.vue
+++ src/components/projects/abstract/ProjectCardItem.vue
@@ -35,8 +35,8 @@ defineProps({
     </p>
 
     <div class="project-links">
-      <router-link v-if="showMoreInfoLink" :to="moreInfoLink">> About</router-link>
-      <a :href="websiteLink" target="_blank">> See</a>
+      <router-link v-if="showMoreInfoLink" :to="moreInfoLink">About</router-link>
+      <a :href="websiteLink" target="_blank">See</a>
     </div>
   </div>
 </template>
