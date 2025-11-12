1cee02c04c5977f6c25be88452e6f557b8caedf5
hayem <marc.haye@etu.umontpellier.fr>
2025-11-12 20:47:36 +0100

    add ProjectImageItem

---
+++ src/components/projects/abstract/ProjectImageItem.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps({
+  link: { type: String, required: true, }
+})
+</script>
+
+<template>
+  <div class="project-image">
+    <a target="_blank" :href="link">
+      <img alt="project-image" :src="link" /></a>
+  </div>
+</template>
