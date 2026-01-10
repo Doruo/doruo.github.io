3dc9cbc4618d8c870a5a6ad9352a87f9b81d67cf
hayem <marc.haye@etu.umontpellier.fr>
2026-01-10 15:59:16 +0100

    simplify imports icons

--- src/data/Icons.ts
+++ src/data/Icons.ts
@@ -1,59 +1,33 @@
-// social
-import portrait_path from '@/assets/img/portrait/portrait.webp'
-import mail_path from '@/assets/img/social/mail.webp'
-import github_path from '@/assets/img/social/github.svg'
-import linkedin_path from '@/assets/img/social/linkedin.webp'
-import gravatar_path from '@/assets/img/social/gravatar.webp'
-import itchio_path from '@/assets/img/social/itchio.webp'
-
-// tools
-import git_path from '@/assets/img/project/tools/git.webp'
-import docker_path from '@/assets/img/project/tools/docker.webp'
-import php_path from '@/assets/img/project/tools/php.svg'
-import html_path from '@/assets/img/project/tools/html.webp'
-import bootstrap_path from '@/assets/img/project/tools/bootstrap.svg'
-import js_path from '@/assets/img/project/tools/js.webp'
-import mysql_path from '@/assets/img/project/tools/mysql.svg'
-import rest_path from '@/assets/img/project/tools/apirest.webp'
-import express_path from '@/assets/img/project/tools/express-js.webp'
-import ldap_path from '@/assets/img/project/tools/ldap.svg'
-import postgre_path from '@/assets/img/project/tools/postgresql.svg'
-import plsql_path from '@/assets/img/project/tools/plsql.svg'
-import chartjs_path from '@/assets/img/project/tools/chartjs.svg'
-import java_path from '@/assets/img/project/tools/java.svg'
-import javafx_path from '@/assets/img/project/tools/javafx.webp'
-import scenebuilder_path from '@/assets/img/project/tools/scenebuilder.webp'
-import go_path from '@/assets/img/project/tools/go.webp'
-import ebitengine_path from '@/assets/img/project/tools/ebitengine.webp'
-import pdf_path from '@/assets/img/project/tools/pdf.svg'
 
 export class SocialIcons {
-  static readonly PORTRAIT = portrait_path
-  static readonly MAIL = mail_path
-  static readonly GITHUB = github_path
-  static readonly LINKEDIN = linkedin_path
-  static readonly GRAVATAR = gravatar_path
-  static readonly ITCHIO = itchio_path
+  static readonly PORTRAIT = new URL('@/assets/img/portrait/portrait.webp', import.meta.url).href
+  static readonly MAIL = new URL('@/assets/img/portrait/portrait.webp', import.meta.url).href
+  static readonly GITHUB = new URL('@/assets/img/social/github.svg', import.meta.url).href
+  static readonly LINKEDIN = new URL('@/assets/img/social/linkedin.webp', import.meta.url).href
+  static readonly GRAVATAR = new URL('@/assets/img/social/gravatar.webp', import.meta.url).href
+  static readonly ITCHIO = new URL('@/assets/img/social/itchio.webp', import.meta.url).href
 }
 
 export class ToolIcons {
-  static readonly GIT = git_path
-  static readonly DOCKER = docker_path
-  static readonly PHP = php_path
-  static readonly HTML = html_path
-  static readonly BOOTSTRAP = bootstrap_path
-  static readonly JS = js_path
-  static readonly MYSQL = mysql_path
-  static readonly REST = rest_path
-  static readonly EXPRESS = express_path
-  static readonly LDAP = ldap_path
-  static readonly POSTGRE = postgre_path
-  static readonly PLSQL = plsql_path
-  static readonly CHARTJS = chartjs_path
-  static readonly JAVA = java_path
-  static readonly JAVAFX = javafx_path
-  static readonly SCENEBUILDER = scenebuilder_path
-  static readonly GO = go_path
-  static readonly EBITENGINE = ebitengine_path
-  static readonly PDF = pdf_path
+  static readonly GIT = new URL('@/assets/img/project/tools/git.webp', import.meta.url).href
+  static readonly DOCKER = new URL('@/assets/img/project/tools/docker.webp', import.meta.url).href
+  static readonly PHP = new URL('@/assets/img/project/tools/php.svg', import.meta.url).href
+  static readonly HTML = new URL('@/assets/img/project/tools/html.webp', import.meta.url).href
+  static readonly BOOTSTRAP = new URL('@/assets/img/project/tools/bootstrap.svg', import.meta.url).href
+  static readonly JS = new URL('@/assets/img/project/tools/js.webp', import.meta.url).href
+  static readonly MYSQL = new URL('@/assets/img/project/tools/mysql.svg', import.meta.url).href
+  static readonly REST = new URL('@/assets/img/project/tools/apirest.webp', import.meta.url).href
+  static readonly EXPRESS = new URL('@/assets/img/project/tools/express-js.webp', import.meta.url).href
+
+
+  static readonly LDAP = new URL('@/assets/img/project/tools/ldap.svg', import.meta.url).href
+  static readonly POSTGRE = new URL('@/assets/img/project/tools/postgresql.svg', import.meta.url).href
+  static readonly PLSQL = new URL('@/assets/img/project/tools/plsql.svg', import.meta.url).href
+  static readonly CHARTJS = new URL('@/assets/img/project/tools/chartjs.svg', import.meta.url).href
+  static readonly JAVA = new URL('@/assets/img/project/tools/java.svg', import.meta.url).href
+  static readonly JAVAFX = new URL('/assets/img/project/tools/javafx.webp', import.meta.url).href
+  static readonly SCENEBUILDER = new URL('@/assets/img/project/tools/scenebuilder.webp', import.meta.url).href
+  static readonly GO = new URL('@/assets/img/project/tools/go.webp', import.meta.url).href
+  static readonly EBITENGINE = new URL('@/assets/img/project/tools/ebitengine.webp', import.meta.url).href
+  static readonly PDF = new URL('@/assets/img/project/tools/pdf.svg', import.meta.url).href
 }
