699bc6f3c417e5e1a9a84425ebc4cd47887dc907
hayem <marc.haye@etu.umontpellier.fr>
2026-05-15 17:07:51 +0200

    update footer links

--- src/components/base/BaseFooter.vue
+++ src/components/base/BaseFooter.vue
@@ -8,14 +8,14 @@ import { SocialIcons } from '@/data/Icons';
   <!--FOOTER-->
   <footer>
     <div class="footer-social">
-      <LinkItem :link="ForgeLinks.PORTFOLIO" :img="SocialIcons.FOOTER_GITHUB" />
+      <LinkItem :link="SocialLinks.GITHUB" :img="SocialIcons.FOOTER_GITHUB" />
       <LinkItem :link="SocialLinks.LINKEDIN" :img="SocialIcons.FOOTER_LINKEDIN" />
-      <LinkItem :link="SocialLinks.DEVTO" :img="SocialIcons.FOOTER_DEVTO" />
+      <LinkItem :link="SocialLinks.DEVTO"    :img="SocialIcons.FOOTER_DEVTO" />
     </div>
     <div class="footer-text">
       <ul>
         <li>&copy; 2024-2026 | All Rights Reserved</li>
-        <li>🦊 Made with <a :href="FrameworkLinks.VUEJS" target="_blank">Vue.js</a></li>
+        <li>🦊 Made with <a :href="FrameworkLinks.VUE" target="_blank">Vue.js</a></li>
       </ul>
     </div>
   </footer>
